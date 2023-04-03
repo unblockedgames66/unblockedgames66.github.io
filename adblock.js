cd31ceba44aaeb094ae7fe314dd1dfb1d2498f08
unblockedgames66 <giakhanh9x@gmail.com>
2023-04-03 16:23:03 +0700

    Update adblock.js

--- adblock.js
+++ adblock.js
@@ -1,17 +1,17 @@
-// Khởi tạo AdGuard API với các cấu hình mặc định và đường dẫn đến danh sách chặn của bạn
-var adguard = new Adguard({
-  filters: ["https://filters.adtidy.org/extension/chromium/filters/3.txt"]
+// Khởi tạo uBlock Origin và đăng ký danh sách EasyList
+var ublock = new UBlock({
+  lists: ['https://easylist-downloads.adblockplus.org/easylist.txt']
 });
 
-// Loại bỏ yêu cầu tải xuống từ trang web "example.com" khỏi danh sách chặn quảng cáo
-adguard.skip("https://example.com/*");
+// Loại bỏ trang web "example.com" khỏi danh sách chặn quảng cáo của uBlock Origin
+ublock.unblock('example.com');
 
-// Lặp qua tất cả các yêu cầu tải xuống từ trang web của bạn và loại bỏ các yêu cầu chứa quảng cáo
+// Lặp qua tất cả các yêu cầu tải xuống từ trang web của bạn và loại bỏ các yêu cầu chứa quảng cáo bằng uBlock Origin
 var blockAds = function() {
   var requests = performance.getEntriesByType("resource");
   requests.forEach(function(request) {
-    if (adguard.matches(request.name)) {
-      adguard.block(request.name);
+    if (ublock.matches(request.name)) {
+      ublock.block(request.name);
     }
   });
 };
